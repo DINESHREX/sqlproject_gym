76ae5e1339cab034f88e82ae970cae48d3fbc561
RADI TECH <dineshselvakumar0312@gmail.com>
2026-03-03 21:21:40 +0530

    scenario commit

--- scenario.sql
+++ scenario.sql
@@ -95,3 +95,63 @@ group by m.m_name, mm.membership_id, i.total_amount having i.total_amount > t_am
 
 select year(payment_date) as year, month(payment_date) as month ,sum(amount) as month_revenue from payments
 group by year(payment_date),month(payment_date) order by year(payment_date),month(payment_date) asc
+
+-- 19 Show members who received refunds.
+
+select m.m_name ,r.payment_id,r.refund_amount from 
+members m join member_memberships mm on m.member_id = mm.member_id 
+join payments p  on p.membership_id = mm.membership_id 
+join refunds r on  p.payment_id = r.payment_id
+
+-- 20 Show total refund amount given
+select sum(refund_amount) as total_refund_amount from refunds
+
+-- 21 Show average payment amount
+
+select avg(amount) from payments
+
+-- 22 Show highest paying member
+
+select m.m_name ,p.amount from members m join member_memberships mm on m.member_id = mm.membership_id
+join payments p on p.membership_id = mm.membership_id order by p.amount desc limit 1
+
+-- 23 Show plan-wise revenue
+select mp.plan_name , sum(mm.plan_price) as total_revenue from membership_plans mp
+join member_memberships mm on mp.plan_id = mm.plan_id
+group by mp.plan_name
+
+-- 24 Show top 3 most profitable membership plans.
+select mp.plan_name , sum(mm.plan_price) as total_revenue from membership_plans mp
+join member_memberships mm on mp.plan_id = mm.plan_id
+group by mp.plan_name order by total_revenue limit 3
+
+-- 25 Show members whose payment is partially completed
+select m.m_name ,i.total_amount,i.status from members m join member_memberships mm 
+on m.member_id = mm.member_id join invoices i on i.membership_id = mm.membership_id having status = 'partial'
+
+-- 26 Show average attendance per member
+
+select m.m_name , avg(a.member_id) from members m join attendance a on m.member_id = a.member_id
+group by m.m_name
+
+
+-- 27 Rank members by total payments (use window function)
+select 
+member_id, m_name,total_payment,
+rank() over (order by (total_payment) desc) as payment_rank
+from(
+select m.member_id,m.m_name, sum(p.amount) as total_payment from members m join member_memberships mm  on m.member_id = mm.member_id
+join payments p on p.membership_id = mm.membership_id group by m.member_id , m.m_name
+) as payment_summary
+
+-- 28  Show trainers who were hired before 2022
+select * from trainers where hire_date < '2022-01-01'
+
+-- 29 Show total number of members per membership plan
+
+select mp.plan_name,count(mm.member_id) as total_no_members from membership_plans mp 
+join member_memberships mm on mp.plan_id = mm.plan_id
+group by mp.plan_name
+
+-- 30 Show total revenue collected per payment method
+select payment_method ,sum(amount) as total_revenue from payments group by payment_method
